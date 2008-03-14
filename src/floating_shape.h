41b844d10182377bc3938d3eff3c86cd92c814ef
nick <nick@a97b1542-0b21-0410-a459-e47997c36f34>
2008-03-14 20:58:40 +0000

    Copyright notice
    git-svn-id: http://svn.nickg.me.uk/work/xcowsay@295 a97b1542-0b21-0410-a459-e47997c36f34

--- src/floating_shape.h
+++ src/floating_shape.h
@@ -1,3 +1,21 @@
+/*  floating_shape.h -- Low-ish level window creation and management.
+ *  Copyright (C) 2008  Nick Gasson
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #ifndef INC_FLOATING_SHAPE_H
 #define INC_FLOATING_SHAPE_H
 
