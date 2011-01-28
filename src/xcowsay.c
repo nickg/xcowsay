81f2505a2cc74326352c88fb6406703239e2d9be
Justin Bogner <justin.bogner@emc.com>
2011-01-28 11:09:59 -0700

    The reading-speed option requires an argument.

--- src/xcowsay.c
+++ src/xcowsay.c
@@ -252,7 +252,7 @@ int main(int argc, char **argv)
    parse_config_file();
    
    int c, index = 0, failure = 0;
-   const char *spec = "hvld:rt:f:";
+   const char *spec = "hvld:r:t:f:";
    const char *dream_file = NULL;
    while ((c = getopt_long(argc, argv, spec, long_options, &index)) != -1) {
       switch (c) {
